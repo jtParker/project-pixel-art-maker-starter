0af82736422ab0573d7c51c0e7d17a7148282fdf
JT Parker <JT@JTs-MacBook-Pro.local>
2018-02-24 23:22:28 -0700

    functionality complete

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
-// Select color input
-// Select size input
+$(document).ready(function() {
 
-// When size is submitted by the user, call makeGrid()
+  // Select color input
 
-function makeGrid() {
+  $('#pixelCanvas').click(function( e ) {
+    var color = $('#colorPicker').val();
+    var target = $( e.target );
+    $(target).css('background-color', color);
+  })
 
-// Your code goes here!
+  // Select size inputWidth
+  var height;
+  var width;
 
-}
+  $(':submit').click(function(e) {
+    e.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+  })
+
+  // When size is submitted by the user, call makeGrid()
+
+  function makeGrid(height, width) {
+    var grid;
+    var cells = '<td></td>'.repeat(width);
+    for (let r = 0; r < height; r++ ) {
+      grid += '<tr>' + cells + '</tr>';
+    }
+    $('#pixelCanvas').append(grid);
+  };
+
+})
